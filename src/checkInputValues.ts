1630445143f59beb418b3f137f146c01d2e59c1e
artimusmaximus <arthurmodymaniv@gmail.com>
2024-07-12 02:03:56 -0700

    changed checkInputValues.ts to accept only letters, backspace and enter, all other values are null

--- src/checkInputValues.ts
+++ src/checkInputValues.ts
@@ -1,28 +1,70 @@
-export function checkInputValues(e) {
+export function checkInputValues(e: string) {
     let inputValue;
     switch (e) {
-        case "SPACE":
-        case "CONTROL":
-        case "CONTROLLEFT":
-        case "ALT":
-        case "ALTLEFT":
-        case "ALTRIGHT":
-        case "TAB":
-        case "SHIFTLEFT":
-        case "SHIFT":
-        case "SHIFTRIGHT":
-        case "ESCAPE":
-        case "DELETE":
-        case "CAPSLOCK":
+        case "A":
+        case "B":
+        case "C":
+        case "D":
+        case "E":
+        case "F":
+        case "G":
+        case "H":
+        case "I":
+        case "J":
+        case "K":
+        case "L":
+        case "M":
+        case "N":
+        case "O":
+        case "P":
+        case "Q":
+        case "R":
+        case "S":
+        case "T":
+        case "U":
+        case "V":
+        case "W":
+        case "X":
+        case "Y":
+        case "Z":
+        case "ENTER":
+        case "BACKSPACE":
         case " ":
-            inputValue = null;
+            inputValue = e.toUpperCase();
             break;
         default:
-            inputValue = e.toUpperCase();
-          break;
+            inputValue = null;
+            break;
       }
     return inputValue;
 }
+
+// OLD
+// export function checkInputValues(e) {
+//     let inputValue;
+//     switch (e) {
+//         case "SPACE":
+//         case "CONTROL":
+//         case "CONTROLLEFT":
+//         case "ALT":
+//         case "ALTLEFT":
+//         case "ALTRIGHT":
+//         case "TAB":
+//         case "SHIFTLEFT":
+//         case "SHIFT":
+//         case "SHIFTRIGHT":
+//         case "ESCAPE":
+//         case "DELETE":
+//         case "CAPSLOCK":
+//         case " ":
+//             inputValue = null;
+//             break;
+//         default:
+//             inputValue = e.toUpperCase();
+//           break;
+//       }
+//     return inputValue;
+// }
 
 // export function checkInputValues(e) {
 //     let inputValue;
