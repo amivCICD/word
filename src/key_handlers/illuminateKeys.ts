d5c13fc54bc18deb4a37d0800a517adc98c941f2
artimusmaximus <arthurmodymaniv@gmail.com>
2025-08-27 21:17:48 -0700

    working on illuminateKeys, issue is deeper though, where enter key is producing an -> for example, also other keys are not even logged, look into it!

--- src/key_handlers/illuminateKeys.ts
+++ src/key_handlers/illuminateKeys.ts
@@ -5,8 +5,8 @@ let arrayOfKeys = Array.from(allKeyboardLetters);
 export function illuminateKeys(letter: string, colorCode: string, reset: any): void {
 
     arrayOfKeys.forEach((key) => {
-        if (key.innerHTML === letter) {
-            // console.log('key.innerHTML, letter', key.innerHTML, letter);
+        if (key.textContent?.trim().toUpperCase() === letter.toUpperCase()) {
+            console.log('key.innerHTML, letter', key.innerHTML, letter);
 
             switch (colorCode) {
                 case "hit":
@@ -27,9 +27,27 @@ export function illuminateKeys(letter: string, colorCode: string, reset: any): v
                     break;
             }
         }
-        arrayOfKeys = arrayOfKeys.filter((key) => key.innerHTML !== letter);
+        // 08 27 2025
+        arrayOfKeys = arrayOfKeys.filter((key) => key.textContent.trim().toUpperCase() !== letter.toUpperCase()); // this is filtering out the letters for later, and then the classes are
+
+        // not being applied to USED letters
+        // if (reset) {
+            //     arrayOfKeys = Array.from(allKeyboardLetters);
+            // }
         if (reset) {
-            arrayOfKeys = Array.from(allKeyboardLetters);
+            // arrayOfKeys = Array.from(allKeyboardLetters);
+            arrayOfKeys = Array.from(allKeyboardLetters).filter(
+                (key) => /^[A-Z]$/i.test(key.textContent?.trim() ?? "")
+            );
+            // arrayOfKeys.forEach(key => {
+            //     key.classList.remove("bg-yellow-200", "bg-green-200", "text-black", "bg-slate-500");
+            //     key.classList.add("text-pink-200", "bg-black");
+            // });
+
         }
+        // else {
+        //     arrayOfKeys = arrayOfKeys.filter((key) => key.innerHTML !== letter);
+        // }
+
     });
 }
