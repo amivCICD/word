7da08123f4f905815ce16eeb99192e8dae77d3e4
artimusmaximus <arthurmodymaniv@gmail.com>
2025-08-28 19:30:20 -0700

    added iconditional statement to typeOutGuess.ts to not attempty to load onMessage before window.WEB_SOCKET_READY

--- src/multiplayer/guess_related/typeOutGuess.ts
+++ src/multiplayer/guess_related/typeOutGuess.ts
@@ -12,52 +12,54 @@ document.addEventListener("DOMContentLoaded", () => {
     arrayOfRowArrays = arrayOfDivRows();
 });
 
-onMessage(async (messageData) => {
-    const state = getGameState();
-    state.arrayOfRowArrays = arrayOfRowArrays;
-    const data = JSON.parse(messageData);
-    if (data.updateType === "backspace" && data.userInput === 'BACKSPACE') {
-        if (state.letterCount >= 0 && state.letterCount < 5) {
-            state.arrayOfRowArrays[state.row][state.rowLetterCount].innerHTML = "";
-            // syncMatrixArrayToServer(state);
-            syncWordRowArrayState(state);
-        } else if (state.letterCount === 5 || state.letterCount === 0) {
-            return;
-        }
-    } else if (data.updateType === 'append') {
-        if (state.letterCount <= 5 && state.rowLetterCount < 6) {
-            state.arrayOfRowArrays[state.row][state.rowLetterCount].innerHTML = state.userInput;
-            // syncMatrixArrayToServer(state);
-            syncWordRowArrayState(state);
-
-        }
-    } else if (data.updateType === "guessAttempt" && data.userInput === "ENTER") {
-        if (state.letterCount === 5) {
-            if (!checkIfWordInWordList(state.guess)) {
-                handleWiggleAnimation(state.arrayOfRowArrays[state.row]);
+if (window.WEB_SOCKET_READY === true) {
+    onMessage(async (messageData) => {
+        const state = getGameState();
+        state.arrayOfRowArrays = arrayOfRowArrays;
+        const data = JSON.parse(messageData);
+        if (data.updateType === "backspace" && data.userInput === 'BACKSPACE') {
+            if (state.letterCount >= 0 && state.letterCount < 5) {
+                state.arrayOfRowArrays[state.row][state.rowLetterCount].innerHTML = "";
+                // syncMatrixArrayToServer(state);
+                syncWordRowArrayState(state);
+            } else if (state.letterCount === 5 || state.letterCount === 0) {
                 return;
             }
-            await handleGuess(state, data, state.gameOver, state.checkCompletionStatus)
-                .then(() => {
-                    state.wordRowArrayState.forEach((row, rowIdx) => {
-                        row.forEach((col, colIdx) => {
-                            if (state.arrayOfRowArrays[rowIdx][colIdx]?.innerHTML === "") {
-                                return;
-                            }
-                            col.class = state.arrayOfRowArrays[rowIdx][colIdx]?.className || "";
-                            col.value = state.arrayOfRowArrays[rowIdx][colIdx]?.innerHTML || "";
-                        })
-                    })
-                    syncNewCss(state.wordRowArrayState);
-                })
-                .then(() => {
-                    swapPlayersFrontEnd(state, false, null);
-                })
-                .catch((e) => console.log(`Error for setting sync word array state in guess attempt\t${e}`));
+        } else if (data.updateType === 'append') {
+            if (state.letterCount <= 5 && state.rowLetterCount < 6) {
+                state.arrayOfRowArrays[state.row][state.rowLetterCount].innerHTML = state.userInput;
+                // syncMatrixArrayToServer(state);
+                syncWordRowArrayState(state);
 
+            }
+        } else if (data.updateType === "guessAttempt" && data.userInput === "ENTER") {
+            if (state.letterCount === 5) {
+                if (!checkIfWordInWordList(state.guess)) {
+                    handleWiggleAnimation(state.arrayOfRowArrays[state.row]);
+                    return;
+                }
+                await handleGuess(state, data, state.gameOver, state.checkCompletionStatus)
+                    .then(() => {
+                        state.wordRowArrayState.forEach((row, rowIdx) => {
+                            row.forEach((col, colIdx) => {
+                                if (state.arrayOfRowArrays[rowIdx][colIdx]?.innerHTML === "") {
+                                    return;
+                                }
+                                col.class = state.arrayOfRowArrays[rowIdx][colIdx]?.className || "";
+                                col.value = state.arrayOfRowArrays[rowIdx][colIdx]?.innerHTML || "";
+                            })
+                        })
+                        syncNewCss(state.wordRowArrayState);
+                    })
+                    .then(() => {
+                        swapPlayersFrontEnd(state, false, null);
+                    })
+                    .catch((e) => console.log(`Error for setting sync word array state in guess attempt\t${e}`));
+
+            }
         }
-    }
-});
+    });
+}
 
 let gameComplete = false;
 export async function typeOutGuess(
